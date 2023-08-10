55995ad2218c020c383c8f6169d9c4e0cc5af0f1
Kevin R. Thornton <krthornt@uci.edu>
2023-08-10 12:10:44 -0700

    add exploratory Python script

---
+++ play.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Segment:
+    left: int
+    right: int
+    node: int
+
+
+def update_overlap(a, left, right, node, anc):
+    ai = anc[a]
+    tleft = max(ai.left, left)
+    tright = min(ai.right, right)
+    rv = 0
+    segright = None
+    print(a, tleft, tright)
+    if ai.right != tright:
+        segright = Segment(tright, ai.right, ai.node)
+        ai.left = tright
+        rv = 1
+    # if segright is not None:
+    out = Segment(left, right, node)
+    print(f"out={out}, ai = {ai}")
+    if out.left == ai.left and out.right == ai.right:
+        print("update mapping")
+        ai.node = out.node
+    else:
+        print("insert out")
+        anc.insert(a, out)
+        #rv -= 1
+    if segright is not None:
+        print(f"segright = {segright}")
+        if segright.left == ai.left and segright.right == ai.right:
+            print("updating node")
+            ai.node = segright.node
+        else:
+            print(f"not equal to {ai.left == segright.left}")
+            anc.insert(a+1, segright)
+        #rv += 1
+
+    # if left == ai.left and right == ai.right:
+    #     ai.node = node
+    # else:
+    #     out = Segment(left, right, node)
+    #     print(f"out = {out}")
+    #     anc.insert(a, out)
+    # rv += 1
+
+    rv = max(rv, 0)
+    print(f"returning {rv} ({a} + {rv} = {a+rv})=> {anc}")
+    return rv
+
+
+def update(anc, overlaps):
+    a = 0
+    o = 0
+
+    while a < len(anc) and o < len(overlaps):
+        oi = overlaps[o]
+        ai = anc[a]
+        if oi.right > ai.left and ai.right > oi.left:
+            a += update_overlap(a, oi.left, oi.right, oi.node, anc)
+            o += 1
+        else:
+            raise NotImplementedError
+            a += 1
+
+    print(f"done: {a} {len(anc)}")
+
+    if len(anc) > a:
+        print(f"truncating to: {anc[:a+1]}")
+        anc[:] = anc[:a + 1]
+
+
+anc = [Segment(0, 2, 0)]
+overlaps = [Segment(0, 1, 2), Segment(1, 2, 1)]
+
+update(anc, overlaps)
+assert sorted(anc, key=lambda x: x.left) == [
+    Segment(0, 1, 2), Segment(1, 2, 1)], f"{anc}"
+
+print("test 2")
+
+anc = [Segment(0, 3, 0)]
+overlaps = [Segment(0, 1, 0), Segment(2, 3, 1)]
+
+update(anc, overlaps)
+assert sorted(anc, key=lambda x: x.left) == overlaps, f"{anc}"
+
+print("test 3")
+
+anc = [Segment(0, 5, 0)]
+overlaps = [Segment(1, 2, 0), Segment(3, 4, 1)]
+
+update(anc, overlaps)
+assert sorted(anc, key=lambda x: x.left) == overlaps, f"{anc}"
