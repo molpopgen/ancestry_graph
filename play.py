5304fc9d645edfbf609a72e9eaf4d47d22a41b19
Kevin R. Thornton <krthornt@uci.edu>
2023-08-11 13:49:40 -0700

    we gotta track leftovers somehow?

--- play.py
+++ play.py
@@ -86,42 +86,48 @@ def update(anc, overlaps):
 
 
 # print("TEST 1")
-# 
+#
 # anc = [Segment(0, 2, 0)]
 # overlaps = [Segment(0, 1, 2), Segment(1, 2, 1)]
-# 
+#
 # update(anc, overlaps)
 # assert sorted(anc, key=lambda x: x.left) == [
 #     Segment(0, 1, 2),
 #     Segment(1, 2, 1),
 # ], f"{anc}"
-# 
+#
 # print("TEST 2")
-# 
+#
 # anc = [Segment(0, 3, 0)]
 # overlaps = [Segment(0, 1, 0), Segment(2, 3, 1)]
-# 
+#
 # update(anc, overlaps)
 # assert sorted(anc, key=lambda x: x.left) == overlaps, f"{anc}"
-# 
+#
 # print("TEST 3")
-# 
+#
 # anc = [Segment(0, 5, 0)]
 # overlaps = [Segment(1, 2, 0), Segment(3, 4, 1)]
-# 
+#
 # update(anc, overlaps)
 # assert sorted(anc, key=lambda x: x.left) == overlaps, f"{anc}"
-# 
+#
 # print("TEST 3")
-# 
+#
 # anc = [Segment(0, 1, 17), Segment(1, 5, 0)]
 # overlaps = [Segment(1, 2, 0), Segment(3, 4, 1)]
-# 
+#
 # update(anc, overlaps)
 # assert sorted(anc, key=lambda x: x.left) == overlaps, f"{anc}"
 
 print("TEST 4")
 anc = [Segment(1, 8, 0), Segment(8, 14, 1), Segment(14, 16, 1)]
+overlaps = [Segment(2, 8, 1), Segment(12, 14, 1)]
+update(anc, overlaps)
+assert sorted(anc, key=lambda x: x.left) == overlaps, f"{anc}"
+
+print("TEST 5")
+anc = [Segment(1, 8, 0), Segment(8, 14, 1), Segment(14, 15, 1), Segment(15, 16, 2)]
 overlaps = [Segment(2, 8, 1), Segment(12, 14, 1)]
 update(anc, overlaps)
 assert sorted(anc, key=lambda x: x.left) == overlaps, f"{anc}"
