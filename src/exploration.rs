e0510c006efd9eb41a06855bf20d763ad4b36e9f
Kevin R. Thornton <krthornt@uci.edu>
2023-08-31 10:39:08 -0700

    update asserts. we have an issues....

--- src/exploration.rs
+++ src/exploration.rs
@@ -1400,14 +1400,14 @@ mod multistep_tests {
 
         // node 1
         validate_edges(1, &[(0, 2, 2), (0, 2, 3)], &graph);
-        let ancestry = extract_ancestry(Node(0), &graph);
+        let ancestry = extract_ancestry(Node(1), &graph);
         assert_eq!(ancestry.len(), 1);
-        for a in [(0, 2, None, 0)] {
+        for a in [(0, 2, Some(0), 1)] {
             assert!(
                 ancestry.contains(&AncestrySegment {
                     left: a.0,
                     right: a.1,
-                    parent: a.2,
+                    parent: a.2.map(Node),
                     mapped_node: Node(a.3)
                 }),
                 "ancestry {:?} not in {ancestry:?}",
@@ -1419,7 +1419,7 @@ mod multistep_tests {
         validate_edges(0, &[(0, 2, 1), (0, 2, 4)], &graph);
         let ancestry = extract_ancestry(Node(1), &graph);
         assert_eq!(ancestry.len(), 1);
-        for a in [(0, 2, Some(0), 1)] {
+        for a in [(0, 2, None, 1)] {
             assert!(
                 ancestry.contains(&AncestrySegment {
                     left: a.0,
