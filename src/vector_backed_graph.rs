cff9b1e3839f2a9165a725b7b329e885e6202dc1
Kevin R. Thornton <krthornt@uci.edu>
2023-10-31 12:12:49 -0700

    setup

--- src/vector_backed_graph.rs
+++ src/vector_backed_graph.rs
@@ -16,6 +16,20 @@ struct Ancestry {
 }
 
 impl Ancestry {
+    fn new_sample(genome_length: i64) -> Self {
+        Self {
+            left: vec![0],
+            right: vec![genome_length],
+            unary_mapping: vec![None],
+        }
+    }
+
+    fn add_ancestry(&mut self, left: i64, right: i64, unary_mapping: Option<Node>) {
+        self.left.push(left);
+        self.right.push(right);
+        self.unary_mapping.push(unary_mapping);
+    }
+
     fn clear(&mut self) {
         self.left.clear();
         self.right.clear();
@@ -50,11 +64,11 @@ struct Tables {
     nodes: Nodes,
 }
 
-#[derive(Default)]
 struct Graph {
     tables: Tables,
     node_heap: NodeHeap,
     current_time: i64,
+    genome_length: i64,
     free_nodes: Vec<usize>,
 }
 
@@ -63,10 +77,36 @@ impl Graph {
         if let Some(index) = self.free_nodes.pop() {
             self.tables.edges[index].clear();
             self.tables.ancestry[index].clear();
+            self.tables.ancestry[index].add_ancestry(0, self.genome_length, None);
             Node(index)
         } else {
             self.tables.nodes.birth_time.push(self.current_time);
+            self.tables
+                .ancestry
+                .push(Ancestry::new_sample(self.genome_length));
+            self.tables.edges.push(Edges::default());
             Node(self.tables.nodes.birth_time.len() - 1)
         }
+    }
+
+    // NOTE: separating this out allows it to be called once
+    // each time a Node is chosen as a parent rather than
+    // once per segment per time chosen as a parent.
+    pub fn enqueue_parent(&mut self, parent: Node) {
+        let h = &mut self.node_heap;
+        if !h.queued_nodes.contains(&parent) {
+            h.node_queue.push(QueuedNode {
+                node: parent,
+                birth_time: self.tables.nodes.birth_time[parent.as_index()],
+            })
+        }
+    }
+
+    // TODO: validate parent/child birth times?
+    pub fn record_transmission(&mut self, left: i64, right: i64, parent: Node, child: Node) {
+        let e = &mut self.tables.edges[parent.as_index()];
+        e.left.push(left);
+        e.right.push(right);
+        e.child.push(child);
     }
 }
